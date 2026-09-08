2df4a351d84c49770e665dc1dcd4e3c4e0c29ab4
vvenv <wenfu.li@qq.com>
2026-09-08 14:39:47 +0800

    chore(build.gradle.kts): increment versionCode from 1 to 2 for app release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.vvenv.tomrun"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         buildConfigField(
             "String",
